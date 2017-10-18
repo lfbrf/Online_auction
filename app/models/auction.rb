0d494c3912ad7a80ce6d94e494f974c41a3ecd6d
Luiz <luizfelipebasile@gmail.com>
2017-10-18 19:27:27 -0200

    Cadastro de Leilao com produtos

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,5 @@
 class Auction < ActiveRecord::Base
-	mount_uploader :image, ImageUploader
-  	has_many :auction_products
-  	has_many :products, :through => :auction_products
-  has_many :tasks, dependent: :destroy
-  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: proc { |att| att['name'].blank? }
+  mount_uploader :image, ImageUploader
+  has_many :auction_products
+  has_many :products, :through => :auction_products
 end
-   
