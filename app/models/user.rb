90472d4a7c2b27cf2e9f4621a6ac77f0f4957eeb
Diego Marczal <dmarczal@gmail.com>
2017-10-25 18:43:18 -0200

    Done some tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  enum role: [:user, :vip, :admin]
+  CAPITALIZE_REGEX = /\A^[A-Z][-a-zA-Z]+$\Z/
+
+  enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
@@ -8,16 +10,19 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable# , :confirmable , :validatable  to use custom validate this was comented
-         validates_presence_of    :password, :on=>:create
-         validates_confirmation_of    :password, :on=>:create
-         validates_length_of    :password, :within => Devise.password_length, :allow_blank => true
-         validates_presence_of    :first_name, :on=>:create
-         validates_presence_of    :last_name, :on=>:create
-         validates_presence_of    :city, :on=>:create
-         validates_presence_of    :email, :on=>:create
-         validates :email, :uniqueness => { :case_sensitive => false } # validate if email is unique.
-validates_format_of :first_name, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
-validates_format_of :last_name, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
-validates_format_of :city, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :trackable, :validatable # to use custom validate this was comented
+
+  validates :first_name, presence: true
+  validates :first_name, :last_name, :city, format: { with: CAPITALIZE_REGEX }
+
+  #validates_presence_of    :first_name, :on=>:create
+  #validates_presence_of    :last_name, :on=>:create
+  #validates_presence_of    :city, :on=>:create
+  #validates_presence_of    :email, :on=>:create
+
+  #validates_format_of :first_name, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
+  #validates_format_of :last_name, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
+  #validates_format_of :city, :with => /\A^[A-Z][-a-zA-Z]+$\Z/
+
 end
