7b71305e129b823a5463fcda6f07006601a7cbc6
Luiz <luizfelipebasile@gmail.com>
2017-11-14 19:49:18 -0200

    a

---
+++ app/controllers/admin/auctions_controller.rb
@@ -0,0 +1,58 @@
+class AuctionsController < ApplicationController
+  before_action :admin_only
+  
+
+  def index
+    @auctions = Auction.all
+  end 
+
+  def new
+    @auction = Auction.new
+  end
+
+  def show
+    @auction = Auction.find(params[:id])
+    @products = Product.all
+  end
+
+  def edit
+    @auction = Auction.find(params[:id])
+  end
+
+  def update
+    @auction = Auction.find(params[:id])
+    if @auction.update_attributes(auction_params)
+       flash[:success] = "Leilao Atualizado"
+      redirect_to @auction
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @auction = Auction.new(auction_params)
+
+    if @auction.save
+      redirect_to @auction
+    else
+      render :new
+    end
+  end
+def destroy
+    auction = Auction.find(params[:id])
+    auction.destroy
+    redirect_to auction, :notice => "Leilao deletado."
+  end
+  private
+
+  def auction_params
+    params.require(:auction).permit(:end_date, :begin_date, :image, :title, {product_ids: []} )
+  end
+
+  def admin_only
+    unless current_user && current_user.admin?
+      redirect_to root_path, :alert => "Acesso Negado."
+    end
+  end
+end
+
---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,58 @@
+  class ProductsController < ApplicationController 
+  before_action :admin_only
+  
+  def index
+    @products = Product.all
+    
+  end
+
+  def new
+   @product = Product.new
+  end
+
+  def show
+   @product = Product.find(params[:id]) 
+  end 
+
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+       flash[:success] = "Produto Atualizado"
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to product, :notice => "Produto deletado."
+  end
+  def create   
+    @product = Product.new(product_params)
+    
+   if @product.save
+      redirect_to @product
+   else 
+      render :new   
+    end
+  end
+  
+private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :state, :description,  {images: []})   
+  end
+  
+  def admin_only
+    unless current_user.admin?
+      redirect_to root_path, :alert => "Acesso Negado."
+    end
+  end
+end 
+
